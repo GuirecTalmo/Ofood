cfc0743d149b1fdf7c28f349d8a25599143ba4a8
Guirec Talmo <guirec.talmo@gmail.com>
2022-06-22 12:08:04 +0200

    Fix : css

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import './styles.scss';
+import "./styles.scss";
 import React, { useState, useRef } from "react";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import { isEmail } from "validator";
 import AuthService from "../../services/auth.service";
-import { useNavigate } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const required = (value) => {
   if (!value) {
@@ -39,8 +39,6 @@ const vpassword = (value) => {
 };
 
 export default function Register() {
-
-
   const form = useRef();
   const checkBtn = useRef();
   const [email, setEmail] = useState("");
@@ -85,51 +83,71 @@ export default function Register() {
   return (
     <div className="register">
       <h1 className="form__creatAccount">Créer un compte</h1>
-      <p className="form__texte">Des recettes crées aux petits oignons pour vous.</p>
       <Form className="form" onSubmit={handleRegister} ref={form}>
         <div className="form__register">
-        <label className="form__email" htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
-                />
+          <label className="form__email" htmlFor="email">
+            Email
+          </label>
+          <Input
+            type="text"
+            className="form-control"
+            name="email"
+            value={email}
+            onChange={onChangeEmail}
+            validations={[required, validEmail]}
+          />
 
-        <label className="form__password" htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
+          <label className="form__password" htmlFor="password">
+            Password
+          </label>
+          <Input
+            type="password"
+            className="form-control"
+            name="password"
+            value={password}
+            onChange={onChangePassword}
+            validations={[required, vpassword]}
+          />
 
           <div className="form__checkbox">
-            <input type="checkbox" required="required" name="checkbox" className="regular-checkbox" />
-            <h2 className="form__cgu">J'ai lu et j'accepte les conditions générales d'utilisation</h2>
+            <input
+              type="checkbox"
+              required="required"
+              name="checkbox"
+              className="regular-checkbox"
+            />
+            <h2 className="form__cgu">
+              J'ai lu et j'accepte les conditions générales d'utilisation
+            </h2>
           </div>
-          <h2 className="form__alreadyRegister">Déjà inscris ? <NavLink to="/users/login"><span className="form__log">Connecte-toi</span></NavLink></h2>
+          <h2 className="form__alreadyRegister">
+            Déjà inscris ?{" "}
+            <NavLink to="/users/login">
+              <span className="form__log">Connecte-toi</span>
+            </NavLink>
+          </h2>
           <div className="form__creatAccount">
-            <Input className="form__registerSubmit" type="submit" value="Créer un compte" />
+            <Input
+              className="form__registerSubmit"
+              type="submit"
+              value="Créer un compte"
+            />
           </div>
         </div>
         {message && (
-            <div className="form-group">
-              <div
-                className={ successful ? "alert alert-success" : "alert alert-danger" }
-                role="alert"
-              >
-                {message}
-              </div>
+          <div className="form-group">
+            <div
+              className={
+                successful ? "alert alert-success" : "alert alert-danger"
+              }
+              role="alert"
+            >
+              {message}
             </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
+          </div>
+        )}
+        <CheckButton style={{ display: "none" }} ref={checkBtn} />
       </Form>
-
     </div>
   );
 }
