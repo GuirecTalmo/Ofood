938a4a3dc8060035a5640a635bfebdbc431c0c5d
Guirec Talmo <guirec.talmo@gmail.com>
2022-08-21 21:38:56 +0200

    Fix : Supp. every comment who's not needed and console.log too.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,8 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { useState, useRef } from "react";
-import propTypes from 'prop-types';
-import AuthService from '../../services/auth.service';
-import './styles.scss';
+import propTypes from "prop-types";
+import AuthService from "../../services/auth.service";
+import "./styles.scss";
 
 export default function Header({ currentUser, setCurrentUser }) {
   const logOut = () => {
@@ -13,19 +13,30 @@ export default function Header({ currentUser, setCurrentUser }) {
   const [isChecked, setCheck] = useState(false);
 
   const toggleMobileNav = () => {
-    setCheck(!isChecked)
+    setCheck(!isChecked);
   };
 
   const logoutProcess = () => {
-    logOut()
+    logOut();
     toggleMobileNav();
   };
 
   return (
     <div className="nav_container">
-      <span className="logo"><img className="logo__img" src="https://i89.servimg.com/u/f89/09/02/66/41/logo-p10.png" alt="Logo Ofood" /></span>
+      <span className="logo">
+        <img
+          className="logo__img"
+          src="https://i89.servimg.com/u/f89/09/02/66/41/logo-p10.png"
+          alt="Logo Ofood"
+        />
+      </span>
       <nav className="nav">
-        <input type="checkbox"  onChange={toggleMobileNav} checked={isChecked} id="toggle-nav" />
+        <input
+          type="checkbox"
+          onChange={toggleMobileNav}
+          checked={isChecked}
+          id="toggle-nav"
+        />
         <label htmlFor="toggle-nav">
           <span className="bar bar-one" />
           <span className="bar bar-two" />
@@ -34,33 +45,37 @@ export default function Header({ currentUser, setCurrentUser }) {
 
         <ul className="nav__list">
           {!currentUser && (
-          <NavLink className="nav__link" to="users/login">
-            <li onClick={toggleMobileNav}>Se connecter</li>
-          </NavLink>
+            <NavLink className="nav__link" to="users/login">
+              <li onClick={toggleMobileNav}>Se connecter</li>
+            </NavLink>
           )}
 
           {!currentUser && (
-          <NavLink className="nav__link" to="users/register">
-            <li onClick={toggleMobileNav}>S'inscrire</li>
-          </NavLink>
+            <NavLink className="nav__link" to="users/register">
+              <li onClick={toggleMobileNav}>S'inscrire</li>
+            </NavLink>
           )}
 
           {currentUser && (
-          <NavLink className="nav__link" to="users/Dashboard">
-            <li onClick={toggleMobileNav}>Dashboard</li>
-          </NavLink>
+            <NavLink className="nav__link" to="users/Dashboard">
+              <li onClick={toggleMobileNav}>Dashboard</li>
+            </NavLink>
           )}
 
           {currentUser && (
-          <NavLink className="nav__link" to="profil/1">
-            <li onClick={toggleMobileNav}>Profil</li>
-          </NavLink>
+            <NavLink className="nav__link" to="profil/1">
+              <li onClick={toggleMobileNav}>Profil</li>
+            </NavLink>
           )}
 
           {currentUser && (
-          <NavLink className="nav__link" to="users/login" onClick={logoutProcess}>
-            <li onClick={toggleMobileNav}>Déconnexion</li>
-          </NavLink>
+            <NavLink
+              className="nav__link"
+              to="users/login"
+              onClick={logoutProcess}
+            >
+              <li onClick={toggleMobileNav}>Déconnexion</li>
+            </NavLink>
           )}
         </ul>
       </nav>
--- client/src/components/NotFound/index.js
+++ client/src/components/NotFound/index.js
@@ -1,9 +1,9 @@
-import './style.scss';
-import Logo from './Fichier 7.svg';
+import "./style.scss";
+import Logo from "./Fichier 7.svg";
 
 export default function NotFound() {
   return (
-      <div className="notFound">
+    <div className="notFound">
       <div className="box">4</div>
       <div className="box">
         <img className="logo" src={Logo} alt="" />
@@ -11,10 +11,13 @@ export default function NotFound() {
       <div className="box">4</div>
       <div className="message">
         <div className="ribbon">
-          <div className="ribbon-content"><p><b>Navré, il n'y a rien de bon à manger sur cette page !</b></p></div>
+          <div className="ribbon-content">
+            <p>
+              <b>Navré, il n'y a rien de bon à manger sur cette page !</b>
+            </p>
+          </div>
         </div>
       </div>
     </div>
-
   );
 }
